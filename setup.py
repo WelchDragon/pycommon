7cfc54e3395348ae7ef2eeaeff093e836c69af5d
WelchDragon <leontat20@gmail.com>
2022-01-20 19:56:11 +0900

    Test14

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='pycommon',
       version='0.1',
       author_email='l.tataurov@bastion-tech.ru',
       description='Default Models for user-service, worker-balance',
-      packages=['pycommon'],      
+      packages=['pycommon','pycommon.models'],      
       zip_safe=False)
