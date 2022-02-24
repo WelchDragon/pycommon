f4ed2bc8b85e09f14200429e347199bfcd6c9bb6
WelchDragon <leontat20@gmail.com>
2022-02-24 21:19:07 +0900

    not+def

--- pycommon/models/order.py
+++ pycommon/models/order.py
@@ -1,12 +1,11 @@
 from email.policy import default
 from enum import Enum
 
-from .base import MoneyField
-from pycommon.base import Money
+from .base import MoneyField, Money
+
 from tortoise import Model, fields
 
 
-default_zero = Money(0)
 class UserOrderSide(str, Enum):
     SELL = "SELL"
     BUY = "BUY"
@@ -36,7 +35,7 @@ class UserOrder(Model):
     price = MoneyField()
     side = fields.CharEnumField(UserOrderSide)
     complete_status = fields.CharEnumField(UserOrderCompleteStatus)    
-    commission = MoneyField(default=default_zero)
+    commission = MoneyField(default=Money(0))
 
     def __str__(self) -> str:
         return f"<{self.id} [amount={self.amount}, price={self.price}, side={self.side}, status={self.complete_status}]>"
